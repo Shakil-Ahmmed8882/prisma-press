e1c3de6c637de7621ad47062e34fc3bf332acced
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2026-06-27 15:12:02 +0600

    fix(comment): clean up route definitions

--- src/modules/comment/comment.routes.ts
+++ src/modules/comment/comment.routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get("/author/:authorId", commentController.getCommentsByAuthor);
 router.get("/:commentId", commentController.getSingleComment);
-router.post("/", auth(Role.ADMIN, ), commentController.createComment);
+router.post("/", auth(Role.ADMIN, Role.USER), commentController.createComment);
 router.patch("/:commentId", auth(Role.USER, Role.ADMIN), commentController.updateComment);
 router.delete("/:commentId", auth(Role.USER, Role.ADMIN), commentController.deleteComment);
 router.patch("/:commentId/moderate", auth(Role.ADMIN), commentController.moderateComment);
